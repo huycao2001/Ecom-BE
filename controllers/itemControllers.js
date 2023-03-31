b94fea8e81206b56b359e61553559fcb4754edbc
unknown <huy.cao2001@hcmut.edu.vn>
2023-03-31 16:57:12 +0700

    Get items

--- controllers/itemControllers.js
+++ controllers/itemControllers.js
@@ -1,17 +1,38 @@
 const Item = require("../models/item.js");
 
-module.exports.get_items = (req, res) => {
-    Item.find()
-        .sort({ date: -1 })
-        .then((items) => res.json(items));
-};
+module.exports.get_items = async (req, res) => {
+    const session = await Item.startSession();
+  
+    try {
+      await session.withTransaction(async () => {
+        const items = await Item.find().sort({ date: -1 }).session(session);
+
+        // const {category} = req.query
+        res.json({
+          message: "Success",
+          items: items,
+        //   category : category
+        });
+      });
+    } catch (error) {
+      console.log(error);
+      return res.json({ message: error.message});
+    } finally {
+      session.endSession();
+    }
+  };
+  
 module.exports.get_item_by_id = (req, res) => {
     const { item_id } = req.body;
     Item.find({ ID: `${item_id}` }).then((item) => res.json(item));
 };
 
 module.exports.post_item = (req, res) => {
-    const newItem = new Item(req.body);
+    // res.json(req.body)
+    const {name, price, badges, imgUrl, category, descriptions, storage_options, color_options,promotion_options, status_options } = req.body;
+
+
+    const newItem = new Item({name, price, badges, imgUrl, category, descriptions, storage_options, color_options,promotion_options, status_options});
     newItem.save().then((item) => res.json(item));
 };
 
--- models/item.js
+++ models/item.js
@@ -1,28 +1,78 @@
 const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
-    Gender: {
+    // Gender: {
+    //     type: String,
+    //     required: true,
+    // },
+    // ID: {
+    //     type: Number,
+    //     required: true,
+    // },
+    // Brand: {
+    //     type: String,
+    //     required: true,
+    // },
+    // Name: {
+    //     type: String,
+    //     required: true,
+    // },
+    // Image: String,
+    // Price_range: {
+    //     type: String,
+    //     required: true,
+    // },
+    // Option: [],
+    name:{
         type: String,
-        required: true,
+        required : true
     },
-    ID: {
-        type: Number,
-        required: true,
+    price:{
+        type : Number,
+        required : true
     },
-    Brand: {
-        type: String,
-        required: true,
+    badges : {
+        type : Array,
+        required : false,
+        default :[]
     },
-    Name: {
-        type: String,
-        required: true,
+    imgUrl:{
+        type : String, 
+        required : true
     },
-    Image: String,
-    Price_range: {
-        type: String,
-        required: true,
+    category: String, 
+
+    descriptions : {
+        type : Array,
+        required : false,
+        default :[]
     },
-    Option: [],
+
+    storage_options : {
+        type : Array,
+        required : false,
+        default :[]
+    },
+    color_options : {
+        type : Array,
+        required : false,
+        default :[]
+    },
+
+    promotion_options : {
+        type : Array,
+        required : false,
+        default :[]
+    },
+
+    status_options : {
+        type : Array,
+        required : false,
+        default :[]
+    }
+
+
+
 });
 
 module.exports = Product = mongoose.model("Product", productSchema);
