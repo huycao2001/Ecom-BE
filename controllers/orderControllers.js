5021c2004e5c69130425b6b8683ea7e4c7e30d12
unknown <huy.cao2001@hcmut.edu.vn>
2023-04-19 17:53:02 +0700

    Momo payment

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -1,8 +1,13 @@
+const { json } = require("express");
 const Order = require("../models/order.js");
 //import Cart from "../models/Cart";
 //import User from "../models/User";
 //import config from "config";
 //const stripe = require('stripe')(config.get('StripeAPIKey'));
+
+var request = require('request');
+
+const crypto = require('crypto');
 
 // Get orders that belong to a user
 module.exports.get_user_orders = async (req, res) => {
@@ -79,6 +84,78 @@ module.exports.update_order = (req, res) => {
         });
     });
 };
+
+
+module.exports.momoCheckout = (req, res) => {
+
+    const partnerCode = "MOMOBKUN20180529"
+    const accessKey = 'klm05TvNBzhg7h7j'
+    const secretKey = "at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa"
+    const amount = "10000";
+    const extraData = "merchantName=MoMo Partner"
+
+    // console.log(redirectHost,ipnHost)
+    const redirectUrl = "localhost:3000";
+    const ipnUrl = "localhost:3000";
+
+    const orderId = "1681813129";
+    const orderInfo = "momo payment"
+    // const requestId = partnerCode + new Date().getTime();
+    const requestId = "1681813126";
+
+    const requestType = "captureMoMoWallet"
+
+    const returnUrl = "https://hk-222-co-3027-e-comm.vercel.app/"; 
+    const notifyUrl = "https://hk-222-co-3027-e-comm.vercel.app/";
+
+    const rawSignature = [
+          `partnerCode=${partnerCode}`,
+          `accessKey=${accessKey}`,
+          `requestId=${requestId}`,
+          `amount=${amount}`,
+          `orderId=${orderId}`,
+          `orderInfo=${orderInfo}`,
+          `returnUrl=${returnUrl}`, 
+          `notifyUrl=${notifyUrl}`, 
+          `extraData=${extraData}`,
+      ].join("&")
+
+      console.log("raw +  " + rawSignature)
+
+
+      var signature = crypto.createHmac('sha256', secretKey)
+      .update(rawSignature)
+      .digest('hex');
+
+      console.log("signature is " + signature);
+// "2b7e6a7fbb6b1effa32d1e07e41161df4b1e4368886c2f51b79528d915d9cf0b"
+    var clientServerOptions = {
+        uri: 'https://test-payment.momo.vn/gw_payment/transactionProcessor',
+        body: JSON.stringify({
+            "partnerCode": partnerCode,
+            "accessKey": accessKey,
+            "requestId": requestId,
+            "amount": amount,            
+            "orderId": orderId,
+            "orderInfo": orderInfo,
+            "returnUrl": returnUrl,
+            "notifyUrl": notifyUrl,
+            "extraData": extraData,
+            "requestType": requestType,
+            "signature": signature
+            
+          }),
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    request(clientServerOptions, function (error, response) {
+        console.log(error,response.body);
+        return res.json(JSON.parse(response.body));
+    });
+};
+
 
 /*module.exports.checkout = async (req,res) => {
     try{
--- routes/order.js
+++ routes/order.js
@@ -6,6 +6,10 @@ router.get("/order/:userId", orderController.get_user_orders);
 router.put("/order/:id", orderController.update_order);
 router.get("/orders", orderController.get_all_orders);
 router.post("/order", orderController.create_order);
+
+
+router.post("/momoCheckout", orderController.momoCheckout);
+
 //router.post("/order/:id", orderController.checkout);
 
 module.exports = router;
